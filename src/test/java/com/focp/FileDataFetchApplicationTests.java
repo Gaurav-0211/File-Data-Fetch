a1fc607bd335d43043eab6f17e6a45c9e7625791
Gaurav Kumar <gauravkr0211@gmail.com>
2025-07-28 10:54:35 +0530

    Initial commit

---
+++ src/test/java/com/focp/FileDataFetchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.focp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileDataFetchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
